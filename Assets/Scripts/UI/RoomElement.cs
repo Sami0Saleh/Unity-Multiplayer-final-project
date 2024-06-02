140802e2280a518b33149fd9ed679cc1b2101683
NeoThePotato <neorayb@gmail.com>
2024-06-02 03:27:25 +0300

    Make Room Element clickable.

--- Assets/Scripts/UI/RoomElement.cs
+++ Assets/Scripts/UI/RoomElement.cs
@@ -1,11 +1,24 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 using Photon.Realtime;
 
 public class RoomElement : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _roomName;
 	[SerializeField] private TextMeshProUGUI _playerCount;
+	[SerializeField] private Button _joinButton;
+
+	private void Start()
+	{
+		_joinButton.onClick.AddListener(JoinButton);
+	}
+
+	private void JoinButton()
+	{
+		PhotonNetwork.JoinRoom(_roomName.text);
+	}
 
 	public void SetProperties(RoomInfo roomInfo)
 	{
