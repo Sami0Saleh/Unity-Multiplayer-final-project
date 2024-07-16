ccd6c58971773fa1aa410b3d6d53004300772169
NeoThePotato <neorayb@gmail.com>
2024-07-16 19:46:39 +0300

    Remove undeeded PhotonView reference from Player.

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -3,52 +3,18 @@ using Photon.Pun;
 
 namespace Game
 {
-	public class Player : MonoBehaviour
+	public class Player : MonoBehaviourPun
 	{
 		[SerializeField] private GameObject _cursorPrefab;
-		[SerializeField, HideInInspector] private PhotonView _photonView;
-
-		[SerializeField] private MeshRenderer _renderer;
-		[SerializeField] private Material[] _colors;
-        private const int RED_MAT_INDEX = 0;
-        private const int GREEN_MAT_INDEX = 1;
-        private const int BLUE_MAT_INDEX = 2;
-        private const int YELLOW_MAT_INDEX = 3;
-
-		private void OnValidate()
-		{
-			_photonView = GetComponent<PhotonView>();
-		}
 
 		private void Awake()
 		{
-			SetColor();
-			if (!_photonView.IsMine)
+			if (!photonView.IsMine)
 			{
 				enabled = false;
 				return;
 			}
 			PhotonNetwork.Instantiate(_cursorPrefab.name, transform.position, transform.rotation);
 		}
-
-        private void SetColor()
-        {
-            var _hashedProperties = _photonView.Owner.CustomProperties;
-			switch (_hashedProperties["PlayerColor"])
-			{
-                case "Red":
-                    _renderer.material = _colors[RED_MAT_INDEX];
-                    break;
-                case "Green":
-                    _renderer.material = _colors[GREEN_MAT_INDEX];
-                    break;
-                case "Blue":
-                    _renderer.material = _colors[BLUE_MAT_INDEX];
-                    break;
-                case "Yellow": _renderer.material = _colors[YELLOW_MAT_INDEX];
-					break;
-
-            }
-        }
-    }
+	}
 }
