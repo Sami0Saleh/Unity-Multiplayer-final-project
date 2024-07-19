13d78bfc2f6eaa27cf8afaa718345508073a7789
NeoThePotato <neorayb@gmail.com>
2024-07-19 03:02:58 +0300

    Set submit input to connect.

--- Assets/Scripts/UI/LoginMenu.cs
+++ Assets/Scripts/UI/LoginMenu.cs
@@ -7,29 +7,27 @@ using WebSocketSharp;
 
 public class LoginMenu : MonoBehaviour
 {
-	[SerializeField] private TMP_InputField _nickname;
+	[SerializeField] private TMP_InputField _nicknameInput;
 	[SerializeField] private Button _connectButton;
 	[SerializeField] private Button _exitButton;
 
-	public string Nickname => _nickname.text;
-
 	private void Start()
 	{
-		_nickname.text = Environment.UserName;
+		_nicknameInput.text = Environment.UserName;
 		_connectButton.onClick.AddListener(ConnectButton);
 		_exitButton.onClick.AddListener(ExitButton);
+		_nicknameInput.onSubmit.AddListener(ConnectButton);
 	}
 
-    private void OnEnable()
-    {
-        ToggleButtonsState(true);
-    }
+	private void OnEnable() => ToggleButtonsState(true);
 
-    public void ConnectButton()
+	public void ConnectButton() => ConnectButton(_nicknameInput.text);
+
+    public void ConnectButton(string nickname)
 	{
-		if (Nickname.IsNullOrEmpty())
+		if (nickname.IsNullOrEmpty())
 			return;
-		PhotonNetwork.LocalPlayer.NickName = Nickname;
+		PhotonNetwork.LocalPlayer.NickName = nickname;
 		PhotonNetwork.ConnectUsingSettings();
 
         ToggleButtonsState(false);
