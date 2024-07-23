9821fefe9840da619d97be980ae659409692a042
NeoThePotato <neorayb@gmail.com>
2024-07-23 20:51:39 +0300

    Remove redundant non-boardgame-related components and behaviors.

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.Events;
 
 namespace Game
 {
 	public class GameManager : MonoBehaviourPunCallbacks
 	{
 		private const int MENU_SCENE_INDEX = 0;
-		private const string GAME_OVER = nameof(GameOver);
+		private const string GAME_OVER = nameof(OnGameOver);
 		public static GameManager Instance;
+
+		public event UnityAction<int> GameOver;
 
 		private List<PlayerCharacter> _activePlayers;
 
@@ -28,25 +31,22 @@ namespace Game
 		public override void OnEnable()
 		{
 			base.OnEnable();
-			PlayerCharacter.PlayerDied += OnPlayerDeath;
+			PlayerCharacter.PlayerEliminated += OnPlayerEliminated;
 			PlayerCharacter.PlayerJoined += OnPlayerJoined;
 		}
 
 		public override void OnDisable()
 		{
 			base.OnDisable();
-			PlayerCharacter.PlayerDied -= OnPlayerDeath;
+			PlayerCharacter.PlayerEliminated -= OnPlayerEliminated;
 			PlayerCharacter.PlayerJoined -= OnPlayerJoined;
 		}
 
 		[PunRPC]
-		public void GameOver(int winningPlayerActorNumber, PhotonMessageInfo info)
+		public void OnGameOver(int winningPlayerActorNumber, PhotonMessageInfo info)
 		{
 			Debug.Assert(info.Sender.IsMasterClient, "Game Over can only be sent by master client");
-			if (PhotonNetwork.LocalPlayer.ActorNumber == winningPlayerActorNumber)
-				Debug.Log("Game Over! You won.");
-			else
-				Debug.Log("Game Over! You lost.");
+			GameOver?.Invoke(winningPlayerActorNumber);
 
 			if (PhotonNetwork.IsMasterClient)
 				StartCoroutine(LeaveMatch());
@@ -57,13 +57,11 @@ namespace Game
 			photonView.RPC(GAME_OVER, RpcTarget.All, winningPlayerActorNumber);
 		}
 
-		private void OnPlayerDeath(PlayerCharacter player)
+		private void OnPlayerEliminated(PlayerCharacter player)
 		{
-			Debug.Log($"{player} eleminated");
 			if (!PhotonNetwork.IsMasterClient)
 				return;
 			_activePlayers.Remove(player);
-			Debug.Log($"{_activePlayers.Count} remaining.");
 			if (_activePlayers.Count <= 1)
 				TriggerGameOver(_activePlayers.Single().ThisPlayer.ActorNumber);
 		}
@@ -72,7 +70,7 @@ namespace Game
 
 		IEnumerator LeaveMatch()
 		{
-			yield return new WaitForSeconds(2f);
+			yield return new WaitForSeconds(2f); // TODO Test without this delay
 			PhotonNetwork.DestroyAll();
 			PhotonNetwork.LoadLevel(MENU_SCENE_INDEX);
 		}
--- Assets/Scripts/Game/PlayerCharacter.cs
+++ Assets/Scripts/Game/PlayerCharacter.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using Photon.Pun;
@@ -8,19 +7,14 @@ namespace Game
 {
 	public class PlayerCharacter : MonoBehaviourPun
 	{
-		private const string RECEIVE_DAMAGE = nameof(ReceiveDamage);
-
 		[SerializeField] private PlayerColors _colorConfig;
 		[SerializeField] private GameObject _cursorPrefab;
 		[SerializeField, HideInInspector] private MeshRenderer _renderer;
 
 		public static event UnityAction<PlayerCharacter> PlayerJoined;
-		public static event UnityAction<PlayerCharacter> PlayerDamaged;
-		public static event UnityAction<PlayerCharacter> PlayerDied;
+		public static event UnityAction<PlayerCharacter> PlayerEliminated;
 
 		public Player ThisPlayer => photonView.Owner;
-		public bool IsDead => HP <= 0;
-		[field: SerializeField] public int HP { get; private set; } = 10;
 
 		private void OnValidate() => _renderer = GetComponentInChildren<MeshRenderer>();
 
@@ -28,9 +22,9 @@ namespace Game
 		{
 			SetMaterial();
 			gameObject.name = ThisPlayer.NickName;
-			if (!photonView.IsMine)
+			if (!photonView.AmOwner)
 				return;
-			//PhotonNetwork.Instantiate(_cursorPrefab.name, transform.position, transform.rotation);
+			PhotonNetwork.Instantiate(_cursorPrefab.name, transform.position, transform.rotation);
 
 			void SetMaterial()
 			{
@@ -42,28 +36,6 @@ namespace Game
 		private void Start()
 		{
 			PlayerJoined?.Invoke(this);
-		}
-
-		public void ReceiveDamage(int damage)
-		{
-			photonView.RPC(RECEIVE_DAMAGE, RpcTarget.All, damage, photonView.Owner.ActorNumber);
-		}
-
-		[PunRPC]
-		private void ReceiveDamage(int damage, int damagedActorNumber)
-		{
-			if (damagedActorNumber != ThisPlayer.ActorNumber)
-				return;
-			HP -= damage;
-			PlayerDamaged?.Invoke(this);
-			Debug.Log(photonView.Owner + " HP Left: " + HP);
-			if (IsDead)
-			{
-				HP = 0;
-				PlayerDied?.Invoke(this);
-				if (photonView.IsMine)
-					PhotonNetwork.Destroy(gameObject);
-			}
 		}
 	}
 }
