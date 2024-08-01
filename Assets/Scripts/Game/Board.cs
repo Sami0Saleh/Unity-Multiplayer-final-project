c8ff6c14708ee5a0a85468140ed0d7b6b2500b6c
NeoThePotato <neorayb@gmail.com>
2024-08-01 05:07:59 +0300

    Implement efficient BoardMask struct.

--- Assets/Scripts/Game/Board.cs
+++ Assets/Scripts/Game/Board.cs
@@ -1,27 +1,93 @@
+using System;
 using UnityEngine;
 
 namespace Game
 {
 	public class Board : MonoBehaviour
 	{
-		const int WIDTH = 9;
-		const int HEIGHT = 7;
+		public const byte WIDTH = 7;
+		public const byte HEIGHT = 9;
+		public const byte MAX_NUMBER_OF_TILES = HEIGHT * WIDTH;
 
 		[SerializeField] private GameObject _tilePrefab;
-		[SerializeField] private Transform _tilesParent;
 		[SerializeField] private Grid _grid;
+		[SerializeField] private Transform _tilesParent;
+		[SerializeField, HideInInspector] private GameObject[] _tiles;
+		[field: SerializeField, HideInInspector] public BoardMask CurrentBoardState { get; private set; }
 
-		[ContextMenu("Create Tiles")]
-		private void CreateTiles()
+		private void OnValidate()
 		{
-			for (int x = 0; x < WIDTH; x++)
+			ValidateCurrentBoardState();
+
+			void ValidateCurrentBoardState()
 			{
-				for (int y = 0; y < HEIGHT; y++)
+				CurrentBoardState = 0;
+				for (byte n = 0; n < _tiles.Length; n++)
 				{
-					var index = new Vector3Int(x, y, 0);
-					Object.Instantiate(_tilePrefab, _grid.CellToWorld(index), Quaternion.identity, _tilesParent);
+					if (_tiles[n] != null)
+						CurrentBoardState |= BoardMask.BitNumberToMask(n);
 				}
 			}
+		}
+
+		[ContextMenu("Recreate Tiles")]
+		private void ReCreateTiles()
+		{
+			_tilesParent.DestroyAllChildrenImmediate();
+			CreateTiles(WIDTH, HEIGHT);
+
+			void CreateTiles(byte width, byte height)
+			{
+				_tiles = new GameObject[MAX_NUMBER_OF_TILES];
+				for (byte y = 0; y < height; y++)
+				{
+					for (byte x = 0; x < width; x++)
+					{
+						CreateTile(x, y);
+					}
+				}
+			}
+
+			GameObject CreateTile(byte x, byte y)
+			{
+				var index = new Vector3Int(x, y, 0);
+				var tile = Instantiate(_tilePrefab, _grid.CellToWorld(index), Quaternion.identity, _tilesParent);
+				tile.name = $"Tile @ {x}, {y}";
+				var bitNumber = BoardMask.IndexToBitNumber(x, y);
+				_tiles[bitNumber] = tile;
+				CurrentBoardState |= BoardMask.BitNumberToMask(bitNumber);
+				return tile;
+			}
+		}
+
+		[Serializable]
+		public struct BoardMask : IEquatable<BoardMask>
+		{
+			[SerializeField] private ulong _mask;
+
+			public bool this[byte x, byte y]
+			{
+				readonly get => (_mask & IndexToMask(x, y)) != 0;
+				set
+				{
+					if (value)
+						_mask |= IndexToMask(x, y);
+					else
+						_mask &= ~IndexToMask(x, y);
+				}
+			}
+
+			public static implicit operator ulong(BoardMask mask) => mask._mask;
+
+			public static implicit operator BoardMask(ulong mask) => new() { _mask = mask };
+
+			public static ulong IndexToMask(byte x, byte y) => BitNumberToMask(IndexToBitNumber(x, y));
+
+			public static ulong BitNumberToMask(byte bitNumber) => (ulong)1 << bitNumber;
+
+			public static byte IndexToBitNumber(byte x, byte y) => (byte)(x + y * WIDTH);
+
+			public readonly bool Equals(BoardMask other) => _mask == other._mask;
 		}
 	}
 }
--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,12 +1,39 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class Utility
+public static class Utility
 {
 	/// <returns>The player number for this player. (0-3)</returns>
 	public static int GetPlayerNumber(Player player) => PhotonNetwork.PlayerList.AsEnumerable().OrderBy(p => p.ActorNumber).ToList().IndexOf(player);
 
 	/// <returns>Like the '%' operator, but can account for negative integers.</returns>
 	public static int Modulo(int a, int b) => ((a %= b) < 0) ? a + b : a;
+
+	/// <summary>
+	/// Calls <see cref="Object.Destroy"/> on all of <paramref name="parent"/>'s children.
+	/// </summary>
+	public static void DestroyAllChildren(this Transform parent)
+	{
+        foreach (var item in parent.GetAllChildren())
+			Object.Destroy(item.gameObject);
+    }
+
+	/// <summary>
+	/// Calls <see cref="Object.DestroyImmediate"/> on all of <paramref name="parent"/>'s children.
+	/// </summary>
+	public static void DestroyAllChildrenImmediate(this Transform parent)
+	{
+		foreach (var item in parent.GetAllChildren())
+			Object.DestroyImmediate(item.gameObject);
+	}
+
+	/// <returns>All children transforms of <paramref name="parent"/>.</returns>
+	public static IEnumerable<Transform> GetAllChildren(this Transform parent)
+	{
+		for (int n = parent.childCount - 1; n >= 0; n--)
+			yield return parent.GetChild(n);
+	}
 }
