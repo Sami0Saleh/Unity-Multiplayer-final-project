30da7df50643419f435327b53fd2d3aafdde266d
NeoThePotato <neorayb@gmail.com>
2024-08-17 12:03:36 +0300

    Fix input.

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Photon.Pun;
 
 namespace Game
@@ -14,7 +15,6 @@ namespace Game
         private Vector3 movementVector = new Vector3();
 
         private Camera _cachedCamera;
-		private Vector3 raycastPos;
 
 
 		private void Awake()
@@ -27,52 +27,37 @@ namespace Game
 			_cachedCamera = Camera.main;
 		}
 
-        private void Update()
+        private void FixedUpdate()
 		{
-			if (photonView.AmOwner)
-            {
-                Ray ray = _cachedCamera.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                    raycastPos = hit.point;
+            movementVector = new Vector3();
+            if (Keyboard.current.wKey.IsPressed())
+                movementVector.z += 1;
+            if (Keyboard.current.sKey.IsPressed())
+                movementVector.z += -1;
+            if (Keyboard.current.dKey.IsPressed())
+                movementVector.x += 1;
+            if (Keyboard.current.aKey.IsPressed())
+                movementVector.x += -1;
+            transform.Translate(speed * Time.fixedDeltaTime * movementVector);
 
-                movementVector = new Vector3();
-                if (Input.GetKey(KeyCode.W))
-                    movementVector.z = 1;
-                if (Input.GetKey(KeyCode.S))
-                    movementVector.z = -1;
-                if (Input.GetKey(KeyCode.D))
-                    movementVector.x = 1;
-                if (Input.GetKey(KeyCode.A))
-                    movementVector.x = -1;
-
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                    Shoot();
-
-                Vector3 directionToFace = raycastPos - gameObject.transform.position;
-                Quaternion lookAtRotation = Quaternion.LookRotation(directionToFace);
-                Vector3 eulerRotation = lookAtRotation.eulerAngles;
-                eulerRotation.x = 0;
-                eulerRotation.z = 0;
-                transform.eulerAngles = eulerRotation;
-                transform.Translate(movementVector * (Time.deltaTime * speed));
-            }
-        
+			if (Pointer.current.press.wasPressedThisFrame)
+				Shoot();
 		}
 
         private void Shoot()
         {
-            if (Physics.Raycast(_cachedCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
-            {
-                if (hit.collider.tag != "Player")
-                    return;
-                var projectile = PhotonNetwork.Instantiate(PROJECTILE_PREFAB, ProjectileSpawnTransform.position, ProjectileSpawnTransform.rotation).GetComponent<Projectile>();
-                projectile.photonView.TransferOwnership(PhotonNetwork.MasterClient);
-            }
-            else
-            {
-                Debug.Log("No valid target, not shooting.");
-            }
-        }
+            var hit = GetWorldPositionOnPlane(Pointer.current.position.ReadValue());
+            hit.y = ProjectileSpawnTransform.position.y;
+			var projectile = PhotonNetwork.Instantiate(PROJECTILE_PREFAB, ProjectileSpawnTransform.position, Quaternion.LookRotation(hit - ProjectileSpawnTransform.position, Vector3.up)).GetComponent<Projectile>();
+            projectile.photonView.TransferOwnership(PhotonNetwork.MasterClient);
+
+			Vector3 GetWorldPositionOnPlane(Vector2 screenPosition, float y = 0)
+			{
+				Ray ray = _cachedCamera.ScreenPointToRay(screenPosition);
+				Plane xz = new(Vector3.up, new Vector3(0, y, 0));
+				xz.Raycast(ray, out float distance);
+				return ray.GetPoint(distance);
+			}
+		}
     }
 }
