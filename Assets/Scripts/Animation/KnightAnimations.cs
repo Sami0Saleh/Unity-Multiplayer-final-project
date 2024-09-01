7bc032f31be22c4e48174ec233507f3c4b51bde0
MatrixVVhite <yotamoscohen@gmail.com>
2024-09-01 22:17:49 +0300

    Added tile dissolving and player walk cycle

--- Assets/Scripts/Animation/KnightAnimations.cs
+++ Assets/Scripts/Animation/KnightAnimations.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class KnightAnimations : MonoBehaviour
 {
     const string POINT_TRIGGER = "TrPoint";
+    const string WALK_TRIGGER = "TrWalk";
+    const string STOP_WALK_TRIGGER = "TrStopWalk";
 
     [SerializeField] private Animator animator;
 
@@ -13,6 +15,26 @@ public class KnightAnimations : MonoBehaviour
         if (animator != null)
         {
             animator.SetTrigger(POINT_TRIGGER);
+        }
+    }
+
+    [ContextMenu("Walk")]
+    public void Walk()
+    {
+        //Rotate character to desired tile
+        if (animator != null)
+        {
+            animator.SetTrigger(WALK_TRIGGER);
+        }
+    }
+
+    [ContextMenu("StopWalk")]
+    public void StopWalk()
+    {
+        //Rotate character to desired tile
+        if (animator != null)
+        {
+            animator.SetTrigger(STOP_WALK_TRIGGER);
         }
     }
 }
---
+++ Assets/Scripts/Animation/TileDissolver.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class TileDissolver : MonoBehaviour
+{
+    private const int DISSOLVE_MAT_INDEX = 1;
+    private const int OUTLINE_MAT_INDEX = 0;
+
+    [SerializeField] private MeshRenderer _renderer;
+
+    private float value = 1;
+    private bool _isDissolving = false;
+
+    private void Start()
+    {
+        value = 1f;
+        _renderer.materials[DISSOLVE_MAT_INDEX].SetFloat("_DissolveValue", value);
+        _renderer.materials[OUTLINE_MAT_INDEX].SetFloat("_Opacity", 1f);
+    }
+
+    private void Update()
+    {
+        if (_isDissolving && value >= 0)
+        {
+            value -= 0.01f;
+            _renderer.materials[DISSOLVE_MAT_INDEX].SetFloat("_DissolveValue", value);
+        }
+        else if (value <= 0)
+        {
+            //Destroy me
+        }
+    }
+
+    [ContextMenu("StartDissolving")]
+    public void StartDissolving()
+    {
+        _isDissolving = true;
+        _renderer.materials[OUTLINE_MAT_INDEX].SetFloat("_Opacity", 0f);
+    }
+}
