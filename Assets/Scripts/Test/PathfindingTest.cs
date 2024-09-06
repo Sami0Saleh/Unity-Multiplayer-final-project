4d0c886a0b4fd9a62f220279c46105ce4bbeaf4e
NeoThePotato <neorayb@gmail.com>
2024-09-06 05:01:43 +0300

    Change PathfindingTest to shake instead of color.

--- Assets/Scripts/Test/PathfindingTest.cs
+++ Assets/Scripts/Test/PathfindingTest.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 using Game;
 
+#if UNITY_EDITOR
 namespace Test
 {
 	public class PathfindingTest : MonoBehaviour
 	{
 		[SerializeField] private byte _x, _y;
 		[SerializeField] private byte _radius = 1;
-		[SerializeField] private Color _color = Color.red;
 
-		[ContextMenu("ShowRadius")]
-		private void ShowRadius()
+		[ContextMenu("Select")]
+		private void Select()
 		{
 			foreach (var tile in Board.Instance.MaskToTiles(Pathfinding.GetArea(_x, _y, _radius)))
-				tile.GetComponentInChildren<MeshRenderer>().material.color = _color;
+				tile.transform.DOShakeScale(1f);
 		}
 	}
 }
+#endif
